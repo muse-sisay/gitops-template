f9939feaef130fcd17e505820b713ae806cc5729
Muse Mulatu <muse@kubefirst.io>
2025-03-13 11:22:56 -0600

    add secondary ip address range

--- aws-github/terraform/aws/eks/vpc.tf
+++ aws-github/terraform/aws/eks/vpc.tf
@@ -8,8 +8,9 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.2"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name                  = local.name
+  cidr                  = local.vpc_cidr
+  secondary_cidr_blocks = [local.pod_cidr]
 
   azs = local.azs
   private_subnets = concat(
